10a22f22d0aa9215b2220d18cfbd9b1b667ed773
emilio alvarado <emiliolvrd@gmail.com>
2017-05-08 13:39:38 -0500

    first exercise completed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,42 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int myFavoriteNumber = 8;
+        System.out.println(myFavoriteNumber);
+        String myString = "my string";
+//        System.out.println(myString);
+//        String myString = 3.14159;
+//        long myNumber;
+//        System.out.println(thisLong);
+//        myNumber = 123;
+//        System.out.println(myNumber);
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+//        int x = 5;
+//
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//            String class = "my class";
+
+//        int x = 4;
+//        x = x + 5;
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+
     }
+
+
 }
