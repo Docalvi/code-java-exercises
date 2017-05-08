3842bc55c0b1a07067f6df135f03fef7dd062b45
emilio alvarado <emiliolvrd@gmail.com>
2017-05-08 15:09:42 -0500

    completed third step in console exercise

--- src/io/consoleExercises.java
+++ src/io/consoleExercises.java
@@ -14,16 +14,21 @@ public class consoleExercises {
 //        System.out.println("You are " + age + " years old");
 
 
-        String firstName;
-        String lastName;
-        String state;
+//        String firstName;
+//        String lastName;
+//        String state;
+//
+//        System.out.println("Enter your first name, last name, and the state you live in: ");
+//        firstName = sc.nextLine();
+//        lastName = sc.nextLine();
+//        state = sc.nextLine();
+//        System.out.println("Your first name is " + firstName + ", your last name is " + lastName + ", the state you live is " + state);
+//
 
-        System.out.println("Enter your first name, last name, and the state you live in: ");
-        firstName = sc.nextLine();
-        lastName = sc.nextLine();
-        state = sc.nextLine();
-        System.out.println("Your first name is " + firstName + ", your last name is " + lastName + ", the state you live is " + state);
-
+        System.out.println("What do you like to do on the weekends?");
+        String answer;
+        answer = sc.next();
+        System.out.println("You like to " + answer);
 
     }
 }
