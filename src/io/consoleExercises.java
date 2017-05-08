3fd471bb7a1c461a2bd52dd3f90a6d1be549fcef
emilio alvarado <emiliolvrd@gmail.com>
2017-05-08 14:55:29 -0500

    completed first step in console exercise

---
+++ src/io/consoleExercises.java
@@ -0,0 +1,23 @@
+package io;
+import java.util.Scanner;
+
+/**
+ * Created by emilioalvarado on 5/8/17.
+ */
+public class consoleExercises {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age;
+        System.out.println("How old are you?");
+        age = sc.nextInt();
+        System.out.println("You are " + age + " years old");
+
+
+
+//        String name;
+//        System.out.println("Enter your name: ");
+//        name = sc.nextLine();
+//        System.out.println("Your name is " + name);
+
+    }
+}
