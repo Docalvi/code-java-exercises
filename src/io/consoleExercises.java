62635f274f96f29fc6dd37f6be471618ec99502c
emilio alvarado <emiliolvrd@gmail.com>
2017-05-09 09:04:04 -0500

    completed console exercise

--- src/io/consoleExercises.java
+++ src/io/consoleExercises.java
@@ -10,34 +10,34 @@ import java.util.Scanner;
 public class consoleExercises {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        int age;
-//        System.out.println("How old are you?");
-//        age = sc.nextInt();
-//        System.out.println("You are " + age + " years old");
+        int age;
+        System.out.println("How old are you?");
+        age = sc.nextInt();
+        System.out.println("You are " + age + " years old");
 
 
-//        String firstName;
-//        String lastName;
-//        String state;
-//
-//        System.out.println("Enter your first name, last name, and the state you live in: ");
-//        firstName = sc.nextLine();
-//        lastName = sc.nextLine();
-//        state = sc.nextLine();
-//        System.out.println("Your first name is " + firstName + ", your last name is " + lastName + ", the state you live is " + state);
-//
+        String firstName;
+        String lastName;
+        String state;
 
-//        System.out.println("What do you like to do on the weekends?");
-//        String answer;
-//        answer = sc.nextLine();
-//        System.out.println("You like to " + answer);
+        System.out.println("Enter your first name, last name, and the state you live in: ");
+        firstName = sc.nextLine();
+        lastName = sc.nextLine();
+        state = sc.nextLine();
+        System.out.println("Your first name is " + firstName + ", your last name is " + lastName + ", the state you live is " + state);
+
+
+        System.out.println("What do you like to do on the weekends?");
+        String answer;
+        answer = sc.nextLine();
+        System.out.println("You like to " + answer);
 
         System.out.println("What is the length and width of the classroom?");
         int length = sc.nextInt();
         int width  = sc.nextInt();
         int area = length * width;
         int perimeter = (2 * length) * (2 * width);
-        System.out.println("the area of the classroom is " + area + " and it's perimeter is " + perimeter);
+        System.out.println("The area of the classroom is " + area + " and it's perimeter is " + perimeter);
 
     }
 }
