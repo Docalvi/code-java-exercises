01e0759ddbab8978d64dfe9a22edcaaf69a17b3b
emilio alvarado <emiliolvrd@gmail.com>
2017-05-18 14:21:06 -0500

    refactored again first step array exercise

---
+++ src/JavaII/RandomName.java
@@ -0,0 +1,53 @@
+package JavaII;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomName {
+
+    public static String adjectives[] = {
+            "Colorful",
+            "Bold",
+            "Wild",
+            "Insightful",
+            "Joyful",
+            "Hectic",
+            "Eager",
+            "Obtuse",
+            "Lovable",
+            "Shady"
+
+    };
+
+    public static String names[] = {
+            "Joey",
+            "John",
+            "Albert",
+            "Ashley",
+            "Valerie",
+            "Louis",
+            "Dom",
+            "Danny",
+            "Marcus",
+            "Steven"
+    };
+    public static String ran(){
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Do you want a random Name?");
+
+        String yesNo = input.nextLine();
+        if  (yesNo.equalsIgnoreCase("y")){
+            int ranadj = random.nextInt(adjectives.length);
+            int ranName = random.nextInt(names.length);
+
+            return  adjectives[ranName] + " - " + names[ranadj];
+
+        } else {
+            return "Alright. Have it your way.";
+        }
+    }
+}
+
+
--- src/JavaII/ServerNameGenerator.java
+++ src/JavaII/ServerNameGenerator.java
@@ -1,57 +1,10 @@
 package JavaII;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class ServerNameGenerator {
 
     public static void main(String[] args) {
 
-        String adjectives[] = {
-                "Colorful",
-                "Bold",
-                "Wild",
-                "Insightful",
-                "Joyful",
-                "Hectic",
-                "Eager",
-                "Obtuse",
-                "Lovable",
-                "Shady"
-
-        };
-
-        String names[] = {
-                "Joey",
-                "John",
-                "Albert",
-                "Ashley",
-                "Valerie",
-                "Louis",
-                "Dom",
-                "Danny",
-                "Marcus",
-                "Steven"
-        };
-
-        Random random = new Random();
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("DO you want a random Name?");
-
-        String yesNo = input.nextLine();
-        if  (yesNo.equalsIgnoreCase("y")){
-            int ranadj = random.nextInt(adjectives.length);
-            int ranName = random.nextInt(names.length);
-
-            System.out.println(adjectives[ranName] + " - " + names[ranadj]);
-
-        }
-
-
-
-
-
+        System.out.println(RandomName.ran());
     }
 
 
