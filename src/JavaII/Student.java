f5a148891fbca8f7032a122445e437f0398e09df
emilio alvarado <emiliolvrd@gmail.com>
2017-05-22 14:27:42 -0500

    Added private properties name and grades Made constructor added methods to get name, add grades and get grade average

---
+++ src/JavaII/Student.java
@@ -0,0 +1,33 @@
+package JavaII;
+import java.util.ArrayList;
+
+
+public class Student {
+
+    private String Name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name, ArrayList<Integer> grades) {
+        Name = name;
+        this.grades = grades;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void addGrades(ArrayList<Integer> grades) {
+        this.grades = grades;
+    }
+
+    public double getGradeAverage(){
+        Integer sum = 0;
+        if(!grades.isEmpty()) {
+            for (Integer mark : grades) {
+                sum += mark;
+            }
+            return sum.doubleValue() / grades.size();
+        }
+        return sum;
+    }
+}
